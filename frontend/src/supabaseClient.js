18bdd5323aabc0f4eceaa1fb5a449fe25e4b6cbe
SMOLYNEUX78 <stephen.molyneux78@gmail.com>
2026-06-04 22:08:04 +0100

    Fix frontend Supabase anon key

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -5,7 +5,7 @@ const supabaseUrl =
   process.env.REACT_APP_SUPABASE_URL ||
   "https://hvtuvapshvhwfgnnhzbh.supabase.co";
 const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJIUzI1NiIsInJlZiI6Imh2dHV2YXBzaHZod2Znbm5oemJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ3MjM4NTMsImV4cCI6MjA2MDI5OTg1M30.rZzCXqkhf93-8o5EYylgaCWxyTxeMzsNvl1lEzeBSpY";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2dHV2YXBzaHZod2Znbm5oemJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ3MjM4NTMsImV4cCI6MjA2MDI5OTg1M30.rZzCXqkhf93-8o5EYylgaCWxyTxeMzsNvl1lEzeBSpY";
 
 const supabase = createClient(
   supabaseUrl,
